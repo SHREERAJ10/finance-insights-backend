fb5c8753fc72aad32a267b5e0928716bff520ca5
Shreeraj Shrestha <shreerajshrestha.dev@gmail.com>
2025-11-17 20:08:59 +0545

    add error handling

--- middleware/verifyToken.js
+++ middleware/verifyToken.js
@@ -1,15 +1,19 @@
 import admin from '../config/firebase.js'
 
 const verifyToken = async (req, res, next)=>{
-    const authHeader = req.headers.authorization;
-    const idToken = authHeader.split(' ')[1];
-    if(!idToken){
-        res.status(401).json({"message":"unauthorized"});
-    }
-    else{
+    try{
+        const authHeader = req.headers.authorization;
+        const idToken = authHeader.split(' ')[1];
+        if(!idToken){
+            res.status(401).json({"message":"unauthorized"});
+        }
         const decodedToken = await admin.auth().verifyIdToken(idToken);
         req.uid = decodedToken.uid;
         next();
+    }
+    catch(err){
+        console.log(err);
+        res.status(500).json({'success':false,'error':'invalid or expired token!'});
     }
 }
 
