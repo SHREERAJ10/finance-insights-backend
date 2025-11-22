b4dcca78546923fc874e364ce80ffc907d3ba2f6
Shreeraj Shrestha <shreerajshrestha.dev@gmail.com>
2025-11-22 14:25:57 +0545

    feat: update income data

--- routes/incomeRoute.js
+++ routes/incomeRoute.js
@@ -2,10 +2,12 @@ import express from 'express';
 import verifyToken from '../middleware/verifyToken.js';
 import addIncomeDataController from '../controller/addIncomeDataController.js';
 import addIncomeSourceController from '../controller/addIncomeSourceController.js';
+import updateIncomeDataController from '../controller/updateIncomeDataController.js';
 
 const incomeRoute = express.Router();
 
 incomeRoute.post('/data',verifyToken, addIncomeDataController);
+incomeRoute.put('/data/:id',verifyToken, updateIncomeDataController);
 incomeRoute.post('/source',verifyToken, addIncomeSourceController);
 
 export default incomeRoute;
---
+++ service/updateIncomeData.js
@@ -0,0 +1,17 @@
+import {PrismaClient} from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+const updateIncomeData = async (incomeId, incomeAmount, incomeSourceId)=>{
+    await prisma.income_Data.update({
+        where:{
+            incomeId:incomeId
+        },
+        data:{
+            incomeAmount:incomeAmount,
+            incomeSourceId:incomeSourceId
+        }
+    });
+}
+
+export default updateIncomeData;
