4875cb68597d1c388fcc9e5d59b5c2d8a774c7e6
Shreeraj Shrestha <shreerajshrestha.dev@gmail.com>
2025-12-06 14:10:31 +0545

    add test routes: incomeToExpenseRatio, savingGoalProgress, spendingLeakDetector

--- routes/insightRoute.js
+++ routes/insightRoute.js
@@ -2,10 +2,14 @@ import express from 'express';
 import verifyToken from '../middleware/verifyToken.js';
 import incomeToExpenseRatioController from '../controller/insightsController/incomeToExpenseRatioController.js';
 import savingGoalProgressController from '../controller/insightsController/savingGoalProgressController.js';
+import spendingLeakDetectorController from '../controller/insightsController/spendingLeakDetectorController.js';
+import getInsightsData from '../middleware/getInsightsData.js';
 
 const insightRoute = express.Router();
 
-insightRoute.get('/incomeToExpenseRatio', verifyToken, incomeToExpenseRatioController);
-insightRoute.get('/savingGoalProgress', verifyToken, savingGoalProgressController);
+insightRoute.get('/incomeToExpenseRatio', verifyToken, getInsightsData, incomeToExpenseRatioController);
+insightRoute.get('/savingGoalProgress', verifyToken, getInsightsData ,savingGoalProgressController);
+insightRoute.get('/spendingLeakDetector', verifyToken, getInsightsData, spendingLeakDetectorController);
+insightRoute.get('/test',verifyToken, getInsightsData);
 
 export default insightRoute;
