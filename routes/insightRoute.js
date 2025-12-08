127d1448c0bfd342d24046077e027ad4961d0b03
Shreeraj Shrestha <shreerajshrestha.dev@gmail.com>
2025-12-08 21:30:22 +0545

    add: endpoint for insightController

--- routes/insightRoute.js
+++ routes/insightRoute.js
@@ -4,12 +4,15 @@ import incomeToExpenseRatioController from '../controller/insightsController/inc
 import savingGoalProgressController from '../controller/insightsController/savingGoalProgressController.js';
 import spendingLeakDetectorController from '../controller/insightsController/spendingLeakDetectorController.js';
 import getInsightsData from '../middleware/getInsightsData.js';
+import paretoStatsController from '../controller/insightsController/paretoStatsController.js';
+import insightController from '../controller/insightsController/insightController.js';
 
 const insightRoute = express.Router();
 
 insightRoute.get('/incomeToExpenseRatio', verifyToken, getInsightsData, incomeToExpenseRatioController);
 insightRoute.get('/savingGoalProgress', verifyToken, getInsightsData ,savingGoalProgressController);
 insightRoute.get('/spendingLeakDetector', verifyToken, getInsightsData, spendingLeakDetectorController);
-insightRoute.get('/test',verifyToken, getInsightsData);
+insightRoute.get('/',verifyToken, getInsightsData, insightController);
+
 
 export default insightRoute;
