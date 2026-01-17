5fc8627b91cd8aef851b12898a46aca7672501cd
Shreeraj Shrestha <shreerajshrestha.dev@gmail.com>
2026-01-17 15:00:44 +0545

    configured cors for hosted frontend

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin:"http://localhost:5173"
+    origin:"https://fynsight.vercel.app/"
 }))
 
 app.use('/user',userRoute);
